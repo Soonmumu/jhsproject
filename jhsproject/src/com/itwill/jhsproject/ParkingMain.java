6330c35d890e308eacac8657a9a5c12b725b571b
Soonmumu <chsohee1537@gmail.com>
2024-11-25 12:01:42 +0900

    Github에서 세번째 커밋!

--- jhsproject/src/com/itwill/jhsproject/ParkingMain.java
+++ jhsproject/src/com/itwill/jhsproject/ParkingMain.java
@@ -5,5 +5,6 @@ public class ParkingMain {
 	public static void main(String[] args) {
 		System.out.println("Hello, Git!");
 		System.out.println("안녕하세요, 깃허브!");
+		System.out.println("깃허브에서 작성됨!");
 	}
 }
